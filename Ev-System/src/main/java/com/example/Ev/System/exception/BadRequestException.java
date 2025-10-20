7669df6eece9c138fe32f37f28d5121338661c6c
TRanDInhTHien-no1 <babakuku1235@gmail.com>
2025-10-20 08:34:15 +0700

    Revert "Test2 3"

--- Ev-System/src/main/java/com/example/Ev/System/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Ev.System.exception;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String message){
-        super(message);
-    }
-}
