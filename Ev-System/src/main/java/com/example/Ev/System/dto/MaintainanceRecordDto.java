041fbb9baf8975d8864d86babeb8bd4705076296
bababubu-no1dev <bababanh1235@gmail.com>
2025-10-22 20:13:52 +0700

    toi da o day

---
+++ Ev-System/src/main/java/com/example/Ev/System/dto/MaintainanceRecordDto.java
@@ -0,0 +1,21 @@
+package com.example.Ev.System.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MaintainanceRecordDto {
+    private String vehicleCondition;
+    private String checklist;
+    private String remarks;
+    private List<PartUsageDto> partsUsed;
+    private List<Integer> staffIds;
+}
