5e62716995f9ab03902b92526aec414da05ccfb1
bababubu-no1dev <bababanh1235@gmail.com>
2025-11-14 04:07:42 +0700

    a

--- Ev-System/src/main/java/com/example/Ev/System/controller/AppointmentController.java
+++ Ev-System/src/main/java/com/example/Ev/System/controller/AppointmentController.java
@@ -154,7 +154,6 @@ public class AppointmentController {
 
         AppointmentResponse response = serviceAppointmentService.getAppointmentDetailsById(id, authentication);
         return ResponseEntity.ok(response);
-        //adasdada
     }
 
     @GetMapping("/staff/{id}")
