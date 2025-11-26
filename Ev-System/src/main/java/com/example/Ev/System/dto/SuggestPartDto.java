c716fcaebd50518a652401630453a17c0f57a8e5
FGApollo <victornhatthinh@gmail.com>
2025-11-26 21:07:29 +0700

    last

--- Ev-System/src/main/java/com/example/Ev/System/dto/SuggestPartDto.java
+++ Ev-System/src/main/java/com/example/Ev/System/dto/SuggestPartDto.java
@@ -10,13 +10,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SuggestPartDto {
     private Integer quantity;
-    private int appointmentId;
+    private Integer appointmentId;
     private String technician_note;
     private String status;
     private Double total_price;
-    private int partId;
+    private Integer partId;
     private String part_name;
     private Double unit_price;
     private String part_description;
-    private Integer part_Id;
+
 }
