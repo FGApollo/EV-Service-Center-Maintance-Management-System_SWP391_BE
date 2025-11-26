b5b54e9320e672a5db4dc1c73b39c6351744c470
bababubu-no1dev <bababanh1235@gmail.com>
2025-11-26 23:33:50 +0700

    a

--- Ev-System/src/main/java/com/example/Ev/System/service/ServiceAppointmentService.java
+++ Ev-System/src/main/java/com/example/Ev/System/service/ServiceAppointmentService.java
@@ -115,6 +115,11 @@ public class ServiceAppointmentService {
         if (oldStatus.equals("in_progress") && newStatus.equals("awaiting_pickup")) {
             return true;
         }
+
+        if (oldStatus.equals("awaiting_pickup") && newStatus.equals("in_progress")) {
+            return true;
+        }
+
         if (oldStatus.equals("awaiting_pickup") && newStatus.equals("completed")) {
             return true;
         }
